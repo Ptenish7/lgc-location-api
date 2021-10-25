265b27ab35fca4e34299262d4435ef0fe8d8df54
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-10-25 22:32:28 +0300

    Implement consumer-producer

---
+++ cmd/lgc-location-api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ozonmp/lgc-location-api/internal/app/retranslator"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+
+	cfg := retranslator.Config{
+		ChannelSize:   512,
+		ConsumerCount: 2,
+		ConsumerSize:  10,
+		ProducerCount: 28,
+		WorkerCount:   2,
+	}
+
+	rt := retranslator.NewRetranslator(cfg)
+	rt.Start()
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigs
+}
