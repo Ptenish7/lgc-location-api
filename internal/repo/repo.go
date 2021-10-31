11f0f761ae3f597260c68fae7fdfa156cb55b2fc
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-10-31 15:32:39 +0300

    Remove panic calls from repo

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -28,20 +28,20 @@ func NewRepo(db *sqlx.DB, batchSize uint) Repo {
 
 // CreateLocation creates a new location
 func (r *repo) CreateLocation(ctx context.Context, latitude float64, longitude float64, title string) (uint64, error) {
-	panic("implement me")
+	return 0, nil
 }
 
 // DescribeLocation returns a location by id
 func (r *repo) DescribeLocation(ctx context.Context, locationID uint64) (*model.Location, error) {
-	panic("implement me")
+	return nil, nil
 }
 
 // ListLocations returns all locations
 func (r *repo) ListLocations(ctx context.Context) ([]*model.Location, error) {
-	panic("implement me")
+	return nil, nil
 }
 
 // RemoveLocation removes a location by id
 func (r *repo) RemoveLocation(ctx context.Context, locationID uint64) (bool, error) {
-	panic("implement me")
+	return false, nil
 }
