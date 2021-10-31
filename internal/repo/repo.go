b1b7d63261b307e75bdd9b538f3f088111749563
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-10-31 13:14:56 +0300

    Create Location repo stubs

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -8,9 +8,12 @@ import (
 	"github.com/ozonmp/lgc-location-api/internal/model"
 )
 
-// Repo is DAO for Template
+// Repo is DAO for Location
 type Repo interface {
-	DescribeTemplate(ctx context.Context, templateID uint64) (*model.Template, error)
+	CreateLocation(ctx context.Context, latitude float64, longitude float64, title string) (uint64, error)
+	DescribeLocation(ctx context.Context, locationID uint64) (*model.Location, error)
+	ListLocations(ctx context.Context) ([]*model.Location, error)
+	RemoveLocation(ctx context.Context, locationID uint64) (bool, error)
 }
 
 type repo struct {
@@ -23,6 +26,22 @@ func NewRepo(db *sqlx.DB, batchSize uint) Repo {
 	return &repo{db: db, batchSize: batchSize}
 }
 
-func (r *repo) DescribeTemplate(ctx context.Context, templateID uint64) (*model.Template, error) {
-	return nil, nil
+// CreateLocation creates a new location
+func (r *repo) CreateLocation(ctx context.Context, latitude float64, longitude float64, title string) (uint64, error) {
+	panic("implement me")
+}
+
+// DescribeLocation returns a location by id
+func (r *repo) DescribeLocation(ctx context.Context, locationID uint64) (*model.Location, error) {
+	panic("implement me")
+}
+
+// ListLocations returns all locations
+func (r *repo) ListLocations(ctx context.Context) ([]*model.Location, error) {
+	panic("implement me")
+}
+
+// RemoveLocation removes a location by id
+func (r *repo) RemoveLocation(ctx context.Context, locationID uint64) (bool, error) {
+	panic("implement me")
 }
