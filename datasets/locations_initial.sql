f033042ef07d3c9250894fe948b432b0d1a1a0e9
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-11-10 18:13:07 +0300

    Create initial datasets

---
+++ datasets/locations_initial.sql
@@ -0,0 +1,6 @@
+insert into locations (latitude, longitude, title, removed)
+values (-13.163060, -72.545560, 'Machu Picchu', false),
+       (20.683060, -88.568610, 'Chichen Itza', false),
+       (41.890169, 12.492269, 'Colosseum', false),
+       (27.174931, 78.042097, 'Taj Mahal', false),
+       (29.979167, 31.134167, 'Great Pyramid of Giza', true);
