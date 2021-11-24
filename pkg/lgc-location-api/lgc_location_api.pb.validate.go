fe7aabef69296677820082a88282f675d773bfd3
Zaurbek Gasanov <zaurbekzet@gmail.com>
2021-11-24 15:01:45 +0300

    Implement sending events to Kafka

--- pkg/lgc-location-api/lgc_location_api.pb.validate.go
+++ pkg/lgc-location-api/lgc_location_api.pb.validate.go
@@ -718,3 +718,98 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RemoveLocationV1ResponseValidationError{}
+
+// Validate checks the field values on LocationEvent with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *LocationEvent) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for Id
+
+	// no validation rules for LocationId
+
+	// no validation rules for Type
+
+	// no validation rules for ExtraType
+
+	// no validation rules for Status
+
+	if v, ok := interface{}(m.GetEntity()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return LocationEventValidationError{
+				field:  "Entity",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if v, ok := interface{}(m.GetUpdatedAt()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return LocationEventValidationError{
+				field:  "UpdatedAt",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	return nil
+}
+
+// LocationEventValidationError is the validation error returned by
+// LocationEvent.Validate if the designated constraints aren't met.
+type LocationEventValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e LocationEventValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e LocationEventValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e LocationEventValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e LocationEventValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e LocationEventValidationError) ErrorName() string { return "LocationEventValidationError" }
+
+// Error satisfies the builtin error interface
+func (e LocationEventValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sLocationEvent.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = LocationEventValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = LocationEventValidationError{}
